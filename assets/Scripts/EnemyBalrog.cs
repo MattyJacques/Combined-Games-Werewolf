28095275d158ca06f89e90a8b11a150c5d6ee728
Lewis Nicholson <Lewis.Nicholson@gradsingames.com>
2016-05-06 21:37:22 +0100

    Balrog - WIP

---
+++ assets/Scripts/EnemyBalrog.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBalrog : Enemy {
+
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
