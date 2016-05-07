f31248d9b3de5ca234af02194a53dc073ab555c6
Lewis Nicholson <Lewis.Nicholson@gradsingames.com>
2016-05-07 19:14:44 +0100

    Balrog Done

--- assets/Scripts/EnemyBalrog.cs
+++ assets/Scripts/EnemyBalrog.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class EnemyBalrog : Enemy {
 
+    public GameObject fireball;
+
+    public float attackingTime = 25f;
+    public float hitTime = 15f;
+
     private Animator anim;
     private Rigidbody2D rb;
 
-    private bool isMoving;
+    private bool isHit;
     private bool isAttacking;
     private float attackTime;
+    public float timeToHit;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        timeToHit = attackingTime;
     }
 
     // Update is called once per frame
@@ -21,22 +28,40 @@ public class EnemyBalrog : Enemy {
     {
         if (health > 0)
         {
-            if ((Vector2.Distance(player.transform.position, transform.position) < 2) && !isAttacking)
+            if ((Vector2.Distance(player.transform.position, transform.position) < rangeCheck) && !isAttacking && timeToHit > 0)
             {
                 isAttacking = true;
                 anim.SetBool("IsAttacking", isAttacking);
                 attackTime = 1.35f;
+                Instantiate(fireball, new Vector3(transform.position.x + 3, transform.position.y + 5, 4.5f), Quaternion.Euler(0,0,-25));
+                
             }
-            else if (Vector2.Distance(player.transform.position, transform.position) < rangeCheck)
+            else if (timeToHit < 0)
             {
-                isMoving = true;
-                anim.SetBool("IsMoving", isMoving);
-                rb.AddForce((player.transform.position - transform.position) * speed * Time.deltaTime);
+                isHit = true;
+                anim.SetBool("IsHit", isHit);
+            }
+
+            if (timeToHit < -hitTime)
+            {
+                timeToHit = attackingTime;
+                isHit = false;
+                anim.SetBool("IsHit", isHit);
             }
             else
             {
-
+                timeToHit -= Time.deltaTime;
             }
+
+            if (attackTime > 0)
+            {
+                attackTime -= Time.deltaTime;
+            }
+            else
+            {
+                isAttacking = false;
+            }
+
         }
         else
         {
