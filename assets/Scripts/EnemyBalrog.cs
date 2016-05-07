6b2af398288878a11d3775d72088902a3f51efb0
Lewis Nicholson <Lewis.Nicholson@gradsingames.com>
2016-05-07 16:06:29 +0100

    Balrog Update

--- assets/Scripts/EnemyBalrog.cs
+++ assets/Scripts/EnemyBalrog.cs
@@ -6,6 +6,10 @@ public class EnemyBalrog : Enemy {
     private Animator anim;
     private Rigidbody2D rb;
 
+    private bool isMoving;
+    private bool isAttacking;
+    private float attackTime;
+
     void Awake()
     {
         anim = GetComponent<Animator>();
@@ -13,7 +17,30 @@ public class EnemyBalrog : Enemy {
     }
 
     // Update is called once per frame
-    void Update () {
-	
-	}
+    void Update()
+    {
+        if (health > 0)
+        {
+            if ((Vector2.Distance(player.transform.position, transform.position) < 2) && !isAttacking)
+            {
+                isAttacking = true;
+                anim.SetBool("IsAttacking", isAttacking);
+                attackTime = 1.35f;
+            }
+            else if (Vector2.Distance(player.transform.position, transform.position) < rangeCheck)
+            {
+                isMoving = true;
+                anim.SetBool("IsMoving", isMoving);
+                rb.AddForce((player.transform.position - transform.position) * speed * Time.deltaTime);
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+
+        }
+    }
 }
