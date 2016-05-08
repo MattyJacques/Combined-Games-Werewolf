3780e875a4aa775730247fcd5666896ff5b981db
Paul Garner <w12014815@northumbria.ac.uk>
2016-05-08 15:20:07 +0100

    Main Menu Scene and code

---
+++ assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+  public enum MenuState { Main, LevelSelect };
+  public MenuState currentState;
+
+  public GameObject StartMenu;
+  public GameObject LevelSelect;
+
+
+
+  // Use this for initialization
+  void Start()
+  {
+    currentState = MenuState.Main;
+    StartMenu.SetActive(true);
+    LevelSelect.SetActive(false);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void OnMainMenu()
+  {
+    StartMenu.SetActive(true);
+  }
+
+  void OnLevelSelect()
+  {
+    LevelSelect.SetActive(true);
+    StartMenu.SetActive(false);
+  }
+
+  public void LoadLevel(string levelName)
+  {
+    SceneManager.LoadScene(levelName);
+    // Application.LoadLevel(levelName);
+  }
+
+
+}
