54a8bc48a2f1378ed7f368d9019d7cf0865031f3
Paul Garner <w12014815@northumbria.ac.uk>
2016-05-12 01:43:30 +0100

    Added projectile and spawner, can be aimed, need to add damage

---
+++ assets/Scripts/PathedProjectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathedProjectile : MonoBehaviour {
+
+  private Transform destination;
+  private float speed;
+
+  public void Initialise(Transform newDestination, float newSpeed)
+  {
+    destination = newDestination;
+    speed = newSpeed;
+  }
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    transform.position = Vector3.MoveTowards(transform.position,
+                                             destination.position,
+                                             Time.deltaTime * speed);
+
+    var distanceSquared = (destination.transform.position - transform.position).sqrMagnitude;
+
+    if (distanceSquared > 0.01f * 0.01f)
+    {
+      return;
+    }
+
+    Destroy(gameObject);
+	}
+}
---
+++ assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+  public Transform destination;
+  public PathedProjectile projectile;
+
+  public float speed;
+  public float fireRate;
+
+  private float nextShotSeconds;
+
+  
+
+	// Use this for initialization
+	public void Start ()
+  {
+    nextShotSeconds = fireRate;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+	  if((nextShotSeconds -= Time.deltaTime) > 0)
+    {
+      return;
+    }
+    nextShotSeconds = fireRate;
+    var newProjectile = (PathedProjectile)Instantiate(projectile, 
+                                                   transform.position,
+                                                   transform.rotation);
+    newProjectile.Initialise(destination, speed);
+	}
+
+  void OnDrawGizmos()
+  {
+    if(destination == null)
+    {
+      return;
+    }
+    Gizmos.color = Color.red;
+    Gizmos.DrawLine(transform.position, destination.position);
+  }
+}
