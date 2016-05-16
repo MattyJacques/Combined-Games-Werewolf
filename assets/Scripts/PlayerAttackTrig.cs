31ebd428982289298133c2189bce14d0448ec548
MattyJacques <iFtw@live.co.uk>
2016-05-16 23:29:45 +0100

    Code quality

--- assets/Scripts/PlayerAttackTrig.cs
+++ assets/Scripts/PlayerAttackTrig.cs
@@ -1,20 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerAttackTrig : MonoBehaviour {
+public class PlayerAttackTrig : MonoBehaviour
+{
 
-  public int damage = 100;                  // How much damage player does
+  private int damage = 100;                  // How much damage player does
 
 	void OnTriggerEnter2D(Collider2D col)
   { // Calls enemy function so enemy within range gets damaged
 
     if (col.CompareTag("Enemy"))
-    { // If collider is from a enemy, call to take damage
-      
+    { // If collider is from a enemy, call to take damage  
       col.SendMessage("TakeDamage", damage);
-   //   col.GetComponent<Enemy>().UpdateHealthBar();
     }
 
   } // OnTriggerEnter2D()
 
-}
+} // PlayerAttackTrig
